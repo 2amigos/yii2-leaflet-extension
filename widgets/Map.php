9f9769a11751baaacaab845fe045463d0521d2d7
Antonio Ramirez <amigo.cobos@gmail.com>
2014-04-22 17:37:36 +0200

    fix height style

--- widgets/Map.php
+++ widgets/Map.php
@@ -11,6 +11,7 @@ use dosamigos\leaflet\LeafLet;
 use dosamigos\leaflet\LeafLetAsset;
 use yii\base\InvalidConfigException;
 use yii\base\Widget;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\helpers\Json;
 
@@ -23,78 +24,86 @@ use yii\helpers\Json;
  */
 class Map extends Widget
 {
-	/**
-	 * @var \dosamigos\leaflet\LeafLet component holding all configuration
-	 */
-	public $leafLet;
-	/**
-	 * @var int the height of the map. Failing to configure the height of the map, will result in
-	 * unexpected results.
-	 */
-	public $height = 200;
-	/**
-	 * @var array the HTML attributes for the widget container tag.
-	 */
-	public $options = [];
+    /**
+     * @var \dosamigos\leaflet\LeafLet component holding all configuration
+     */
+    public $leafLet;
+    /**
+     * @var int the height of the map. Failing to configure the height of the map, will result in
+     * unexpected results.
+     */
+    public $height = 200;
+    /**
+     * @var array the HTML attributes for the widget container tag.
+     */
+    public $options = [];
 
-	/**
-	 * Initializes the widget.
-	 * This method will register the bootstrap asset bundle. If you override this method,
-	 * make sure you call the parent implementation first.
-	 */
-	public function init()
-	{
-		parent::init();
-		if (!isset($this->options['id'])) {
-			$this->options['id'] = $this->getId();
-		}
-		if (empty($this->leafLet) || !($this->leafLet instanceof LeafLet)) {
-			throw new InvalidConfigException("'leafLet' attribute cannot be empty and should be of type LeafLet component.");
-		}
-	}
+    /**
+     * Initializes the widget.
+     * This method will register the bootstrap asset bundle. If you override this method,
+     * make sure you call the parent implementation first.
+     */
+    public function init()
+    {
+        parent::init();
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->getId();
+        }
+        if (empty($this->leafLet) || !($this->leafLet instanceof LeafLet)) {
+            throw new InvalidConfigException("'leafLet' attribute cannot be empty and should be of type LeafLet component.");
+        }
+        $inlineStyles = ArrayHelper::getValue($this->options, 'style');
+        if ($inlineStyles) {
+            $styles = explode(';', $inlineStyles);
+            $styles[] = "height:{$this->height}px";
+            $this->options['style'] = implode(";", $styles);
+        } else {
+            $this->options['style'] = "height:{$this->height}px;";
+        }
+    }
 
-	/**
-	 * Renders the map
-	 * @return string|void
-	 */
-	public function run()
-	{
-		echo "\n" . Html::tag('div', '', $this->options);
-		$this->registerScript();
-	}
+    /**
+     * Renders the map
+     * @return string|void
+     */
+    public function run()
+    {
+        echo "\n" . Html::tag('div', '', $this->options);
+        $this->registerScript();
+    }
 
-	/**
-	 * Register the script for the map to be rendered according to the configurations on the LeafLet
-	 * component.
-	 */
-	public function registerScript()
-	{
-		$view = $this->getView();
+    /**
+     * Register the script for the map to be rendered according to the configurations on the LeafLet
+     * component.
+     */
+    public function registerScript()
+    {
+        $view = $this->getView();
 
-		LeafLetAsset::register($view);
-		$this->leafLet->getPlugins()->registerAssetBundles($view);
+        LeafLetAsset::register($view);
+        $this->leafLet->getPlugins()->registerAssetBundles($view);
 
-		$id = $this->options['id'];
-		$name = $this->leafLet->name;
-		$js = $this->leafLet->getJs();
+        $id = $this->options['id'];
+        $name = $this->leafLet->name;
+        $js = $this->leafLet->getJs();
 
-		$clientOptions = $this->leafLet->clientOptions;
-		if ($clientOptions !== false) {
-			$options = empty($clientOptions) ? '{}' : Json::encode($clientOptions);
-			array_unshift($js, "var $name = L.map('$id', $options);");
-			if ($this->leafLet->getTileLayer() !== null) {
-				$js[] = $this->leafLet->getTileLayer()->encode();
-			}
-		} else {
-			return;
-		}
-		$clientEvents = $this->leafLet->clientEvents;
+        $clientOptions = $this->leafLet->clientOptions;
+        if ($clientOptions !== false) {
+            $options = empty($clientOptions) ? '{}' : Json::encode($clientOptions);
+            array_unshift($js, "var $name = L.map('$id', $options);");
+            if ($this->leafLet->getTileLayer() !== null) {
+                $js[] = $this->leafLet->getTileLayer()->encode();
+            }
+        } else {
+            return;
+        }
+        $clientEvents = $this->leafLet->clientEvents;
 
-		if (!empty($clientEvents)) {
-			foreach ($clientEvents as $event => $handler) {
-				$js[] = "$name.on('$event', $handler);";
-			}
-		}
-		$view->registerJs("function {$name}_init(){\n" . implode("\n", $js) . "}\n{$name}_init();");
-	}
+        if (!empty($clientEvents)) {
+            foreach ($clientEvents as $event => $handler) {
+                $js[] = "$name.on('$event', $handler);";
+            }
+        }
+        $view->registerJs("function {$name}_init(){\n" . implode("\n", $js) . "}\n{$name}_init();");
+    }
 } 
