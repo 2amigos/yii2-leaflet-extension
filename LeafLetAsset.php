540b5b4dc78427ed16c75ed9289261ed07317941
Albert Krewinkel <albert@zeitkraut.de>
2015-01-21 11:56:16 +0100

    Update LeafLet files to version 0.7.3
    This also updates the documentation of LeafLetAsset which describes
    how to a CDN as an alternative to local assets.

--- LeafLetAsset.php
+++ LeafLetAsset.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright Copyright (c) 2013 2amigOS! Consulting Group LLC
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
  * @link http://2amigos.us
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
@@ -18,8 +18,8 @@ use yii\web\AssetBundle;
  * 			'bundles' => [
  * 				'dosamigos\leaftlet\LeafLetAsset' => [
  * 					'sourcePath' => null,
- * 					'js' => [ 'http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.js' ],
- * 					'css' => [ 'http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.css' ]
+ * 					'js' => [ 'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js' ],
+ * 					'css' => [ 'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css' ]
  * 				]
  * 			]
  * 		]
